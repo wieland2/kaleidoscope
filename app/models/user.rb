b824bbe64050a705bed4bb0638bb93c44f98d31c
Wagner Wieland <wagnerp5@hotmail.com>
2023-04-01 15:57:12 -0300

    added columns to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
   has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :bio, length: { maximum: 120}
 end
